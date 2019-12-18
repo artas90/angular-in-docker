210f566459ab1665373f1a3ffde9207a8a94e4a9
Andrey Sokolov <artas90@gmail.com>
2019-12-18 11:03:49 +0200

    Add docker example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
@@ -10,6 +11,10 @@ import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.com
 import { TodoDataService } from './todo-data.service';
 import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 import { ApiService } from './api.service';
+import { ROUTES } from './app.routing';
+import { MainComponent } from './main-page/app.component';
+import { Page1Component } from './page-1/page-1.component';
+import { Page2Component } from './page-2/page-2.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +22,16 @@ import { ApiService } from './api.service';
     TodoListComponent,
     TodoListFooterComponent,
     TodoListHeaderComponent,
-    TodoListItemComponent
+    TodoListItemComponent,
+    MainComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [TodoDataService, ApiService],
   bootstrap: [AppComponent]
